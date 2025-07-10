ce31b9ac524855cb89fd577619db20e45973cb78
matheus050600 <theuzinobrabo2.0@gmail.com>
2025-07-10 20:23:49 -0300

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,25 +1,23 @@
 
-self.addEventListener('install', function(event) {
-    event.waitUntil(
-        caches.open('simulador-financeiro-v1').then(function(cache) {
-            return cache.addAll([
-                '/',
-                '/index.html',
-                '/style.css',
-                '/app.js',
-                '/manifest.json',
-                '/icon-192.png',
-                '/icon-512.png'
-            ]);
-        })
-    );
-    self.skipWaiting();
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open('simulador-cache-v1').then(cache => {
+      return cache.addAll([
+        './',
+        './index.html',
+        './style.css',
+        './app.js',
+        './icon-192.png',
+        './icon-512.png'
+      ]);
+    })
+  );
 });
 
-self.addEventListener('fetch', function(event) {
-    event.respondWith(
-        caches.match(event.request).then(function(response) {
-            return response || fetch(event.request);
-        })
-    );
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
+  );
 });
